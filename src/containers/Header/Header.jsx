db6c75697b7a560bcd7dcf404975555746a20a09
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-10 14:22:24 +0100

    Header done

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import './Header.css'
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
 const Header = () => {
   return (
-    <div>
-      
+    <div className='app__header app__wrapper section__padding'id='home'>
+      <div className='app__wrapper_info'>
+ <SubHeading title="Chase the new flavour"/>
+ <h1 className='app__header-h1'>The Key to Fine Pizza</h1>
+ <p className='p__opensans' style={{ margin: '2rem 0' }}>every pizza is a culinary adventure, pairing wood-fired perfection with the freshest ingredients. Taste the tradition reimagined in every slice.</p>
+  <button  type="button" className='custom__button'>Explore Menu</button>
+      </div>
+      <div className='app__wrapper_img'>
+        <img src={images.welcome}alt="welcome" style={{width:'70%'}}/>
+      </div>
     </div>
   )
 }

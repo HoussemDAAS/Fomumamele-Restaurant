998078a1d10e3f92531ccf57c01c577f7ed4a5ec
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-11 15:48:22 +0100

    Footer completed

--- src/components/index.js
+++ src/components/index.js
@@ -2,4 +2,6 @@ import SubHeading from './SubHeading/SubHeading';
 import Footer from './Footer/Footer';
 import NavBar from './NavBar/NavBar';
 import MeniItem from './menuItem/MeniItem';
-export { SubHeading, Footer, NavBar, MeniItem};
+import FooterOverlay from './FooterOverlay/FooterOverlay';
+import Newsletter from './Newsletter/Newsletter';
+export { SubHeading, Footer, NavBar, MeniItem, FooterOverlay, Newsletter};

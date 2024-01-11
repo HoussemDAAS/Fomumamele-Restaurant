cdfe84e4db558065e758115af95bb0de4d632aa9
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-11 11:26:08 +0100

    Video add

--- src/containers/Intro/Intro.jsx
+++ src/containers/Intro/Intro.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './Intro.css'
+import {BsFillPlayFill,BsPauseFill} from 'react-icons/bs'
+import {meal} from '../../constants'
 const Intro = () => {
+const myVideo = React.useRef();
+ const [play,setplay]= useState(false);
+ const handleVideo =()=>{
+  setplay((prevplay)=>!prevplay);
+  if(play){
+    myVideo.current.pause();
+  }
+  else{
+    myVideo.current.play();
+  }
+ }
   return (
-    <div>
-      
+    <div className='app__video'>
+      <video  src={meal}loop autoPlay type="video/mp4" controls={false}muted ref={myVideo}/>
+      <div className='app__video-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center' onClick={handleVideo}>
+          {play
+          ?(<BsPauseFill color='#fff' fontSize={30}/>)
+          :(<BsFillPlayFill color='#fff'fontSize={30}/>)} 
+        </div>
+      </div>
     </div>
   )
 }

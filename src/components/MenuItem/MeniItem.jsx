3cf98747a9f23a3f667ef95a9168e04a15e966f3
HoussemDAAS <houssemdaas2@gmail.com>
2024-01-11 10:14:27 +0100

    Menu done

--- src/components/MenuItem/MeniItem.jsx
+++ src/components/MenuItem/MeniItem.jsx
@@ -2,20 +2,21 @@ import React from 'react'
 import  './MenuItem.css'
 const MeniItem = ({title,price,tags}) => {
   return (
-    <div className='app__menuitem'>
-      <div className='app__menueitem-head'>
-        <div className='app__menuitem-name'>
-          <p className='p__cormorant'  style={{color:"#DCCA87"}}>{title}</p> 
-        </div>
-        <div  className='app__menuitem-dash'/>
-        <div className='app__menuitem-price'>
-          <p className='p__cormorant'>{price}</p> 
-        </div>
+    <div className="app__menuitem">
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{ color: '#ead181s' }}>{title}</p>
       </div>
-      <div className='app__menuitem-sub'>
-        <p className='p__opensans' style={{color:"#AAA"}}>{tags}</p>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__cormorant"style={{ color: '#AAAAAA' }}>{price}</p>
       </div>
     </div>
+
+    <div className="app__menuitem-sub">
+      <p className="p__opensans" style={{ color: '#AAAAAA' }}>{tags}</p>
+    </div>
+  </div>
   )
 }
 
--- src/containers/Menu/Menu.jsx
+++ src/containers/Menu/Menu.jsx
@@ -4,38 +4,39 @@ import { SubHeading,MeniItem} from '../../components'
 import { images, data } from '../../constants'
 const Menu = () => {
   return (
-    <div className='app__menu flex__center section__padding' id='menu'>
-      <div className='app__menu-title'>
-        <SubHeading title="Menu that fits you desire" />
-        <h1 className='headtext__cormorant'>
-          Today's Specials
-        </h1>
-      </div>
-      <div className='app__menu-menu'>
-        <div className='app__menu_menu_italia flex__center'>
-          <p className='app__menu_menu_heading'>Italian Pizza</p>
-          <div className='app__menu_menu_items'>
-            {data.italia.map((item, index) => (
-              <MeniItem key={item.title + index} title={item.title} price={item.price} tags={item.tags}/>
-            ))}
-          </div>
-        </div>
-        <div className='app__menu_menu_img'>
-          <img src={images.menu} alt="menu img" />
-        </div>
-        <div className='app__menu_menu_special flex__center'>
-          <p className='app__menu_menu_heading'>Special Pizza</p>
-          <div className='app__menu_menu_items'>
-            {data.Speicial.map((item, index) => (
-               <MeniItem key={item.title + index} title={item.title} price={item.price} tags={item.tags}/>
-            ))}
-          </div>
-        </div>
-      </div>
-      <div style={{marginTop: '15px' }}>
-        <button type='button' className='custom__button'>View More</button>
-      </div>
+    <div className="app__specialMenu flex__center section__padding" id="menu">
+    <div className="app__specialMenu-title">
+      <SubHeading title="Menu that fits your palatte" />
+      <h1 className="headtext__cormorant">Today's Special</h1>
     </div>
+
+    <div class="app__specialMenu-menu">
+  <div class="app__specialMenu-menu_pizza flex__center">
+    <p class="app__specialMenu-menu_heading">Special</p>
+    <div class="app__specialMenu_menu_items">
+      {data.Speicial.map((item, index) => (
+        <MeniItem key={item.title + index} title={item.title} price={item.price} tags={item.tags} />
+      ))}
+    </div>
+  </div>
+
+  <div class="app__specialMenu-menu_img">
+    <img src={images.menu} alt="menu__img" />
+  </div>
+
+  <div class="app__specialMenu-menu_pizza flex__center">
+    <p class="app__specialMenu-menu_heading">Italian Pizza</p>
+    <div class="app__specialMenu_menu_items">
+      {data.italia.map((item, index) => (
+        <MeniItem key={item.title + index} title={item.title} price={item.price} tags={item.tags} />
+      ))}
+    </div>
+  </div>
+</div>
+
+<div style={{ marginTop: 15 }}>
+  <button type="button" class="custom__button">View More</button>
+</div></div>
   )
 }
 
